a80638c767a64d80cbf454619e55b9118c36be9c
Alex <80858832+Humanoidear@users.noreply.github.com>
2025-07-16 12:37:23 +0200

    Fix: Added missing japanese localization.

--- countries.go
+++ countries.go
@@ -4,7 +4,7 @@ func (n *News) GetTopicsForLanguage() []string {
 	switch n.currentLanguageCode {
 	case 0:
 		// TODO: I need jess to alive
-		return []string{"社会", "国際", "スポーツ", "芸能文化", "経済", "Science", "Technology"}
+		return []string{"社会", "国際", "スポーツ", "芸能文化", "経済", "科学", "テクノロジー"}
 	case 2:
 		return []string{"Deutschland", "Weltnachrichten", "Sport", "Unterhaltung", "Wirtschaft", "Gesundheit/Medizin", "Technik"}
 	case 3:
