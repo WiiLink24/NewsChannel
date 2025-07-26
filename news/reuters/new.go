cd676d7aa8f9b80ab9ee4fa32c21a80d3a65c38d
Harry Walker <contact@harrywalker.uk>
2025-07-26 21:45:32 +0100

    feat: all reuters countries

--- news/reuters/new.go
+++ news/reuters/new.go
@@ -9,13 +9,29 @@ import (
 type Country string
 
 const (
-	UnitedStates  = "us"
+	Brazil        = "brazil"
 	Canada        = "canada"
-	Germany       = "germany"
-	France        = "france"
-	UnitedKingdom = "uk"
+	Mexico        = "mexico"
+	Peru          = "peru"
+	UnitedStates  = "us"
+	Uruguay       = "uruguay"
 	Australia     = "australia"
+	France        = "france"
+	Germany       = "germany"
+	Lithuania     = "lithuania"
+	Mozambique    = "mozambique"
 	Poland        = "poland"
+	Russia        = "russia"
+	SouthAfrica   = "south-africa"
+	UnitedKingdom = "uk"
+	Azerbaijan    = "azerbaijan"
+	Sudan         = "sudan"
+	Taiwan        = "taiwan"
+	SouthKorea    = "south-korea"
+	Singapore     = "singapore"
+	China         = "china"
+	India         = "india"
+	Syria         = "syria"
 )
 
 type Reuters struct {
@@ -36,18 +52,50 @@ func NewReuters(oldArticleTitles []string, countryCode uint8) *Reuters {
 
 func getCountry(code uint8) Country {
 	switch code {
-	case 65:
-		return Australia
-	case 78:
-		return Germany
-	case 77:
-		return France
-	case 97:
-		return Poland
+	case 16:
+		return Brazil
 	case 18:
 		return Canada
+	case 36:
+		return Mexico
+	case 42:
+		return Peru
+	case 50:
+		return Uruguay
+	case 65:
+		return Australia
+	case 77:
+		return France
+	case 78:
+		return Germany
+	case 87:
+		return Lithuania
+	case 92:
+		return Mozambique
+	case 97:
+		return Poland
+	case 100:
+		return Russia
+	case 104:
+		return SouthAfrica
 	case 110:
 		return UnitedKingdom
+	case 113:
+		return Azerbaijan
+	case 118:
+		return Sudan
+	case 128:
+		return Taiwan
+	case 136:
+		return SouthKorea
+	case 153:
+		return Singapore
+	case 160:
+		return China
+	case 169:
+		return India
+	case 175:
+		return Syria
 	default:
 		return UnitedStates
 	}
