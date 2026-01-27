efbafa30996da2845d26494b5c7b8ecfcba91c4d
Harry Walker <contact@harrywalker.uk>
2026-01-27 22:20:54 +0000

    chore: correct category names now matching nintendo's, according to https://tcrf.net/News_Channel

--- countries.go
+++ countries.go
@@ -2,20 +2,19 @@ package main
 
 func (n *News) GetTopicsForLanguage() []string {
 	switch n.currentLanguageCode {
-	// TODO: figure out why these category names break the channel
-	// case 0:
-	// 	return []string{"社会", "国際", "スポーツ", "芸能文化", "経済", "科学", "テクノロジー"}
-	// case 2:
-	// 	return []string{"Deutschland", "Weltnachrichten", "Sport", "Unterhaltung", "Wirtschaft", "Gesundheit/Medizin", "Technik"}
+	case 0:
+		return []string{"社会", "国際", "スポーツ", "芸能文化", "経済", "科学", "テクノロジー"}
+	case 2:
+		return []string{"Inland", "Weltweit", "Sport", "Feuilleton", "Wirtschaft", "Wissenschaft/Gesundheit", "Technologie"}
 	case 3:
-		return []string{"France", "Monde", "Sports", "Culture", "Economie", "Sciences et santé", "Technologies"}
+		return []string{"Actualités nationales", "Actualités internationales", "Sport", "Arts & loisirs", "Economie", "Sciences & santé", "Technologie"}
 	case 4:
-		return []string{"España", "Internacional", "Deportes", "Cultura", "Economia", "Ciencia y salud", "Tecnologia"}
+		return []string{"Nacional", "Internacional", "Deportes", "Cultura y ocio", "Economia", "Ciencia y salud", "Tecnologia"}
 	case 5:
-		return []string{"Dall'Italia", "Dal mondo", "Sport", "Cultura", "Economia", "Scienza", "Tecnologia"}
+		return []string{"Notizie nazionali", "Notizie internazionali", "Sport", "Arte e Intrattenimento", "Attività commerciale", "Scienze e salute", "Tecnologia"}
 	case 6:
 		return []string{"Continentaal nieuws", "Internationaal nieuws", "Sport", "Kunst en entertainment", "Zakelljk nieuws", "Wetenschap en gezondheid", "Technologie"}
 	default:
-		return []string{"National News", "International News", "Sports", "Entertainment", "Business", "Science", "Technology"}
+		return []string{"National News", "International News", "Sports", "Arts/Entertainment", "Business", "Science/Health", "Technology"}
 	}
 }
